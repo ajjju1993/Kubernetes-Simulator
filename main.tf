f710b8d104b79862e95b8a3c08c201f633bef05d
ajjju1993 <ajinkyaindulkar.005@gmail.com>
2024-07-11 18:06:32 -0500

    skipping the secrets manager part

--- main.tf
+++ main.tf
@@ -31,23 +31,13 @@ resource "aws_s3_bucket_lifecycle_configuration" "terraform_state_lifecycle" {
   }
 }
 
-data "aws_secretsmanager_secret" "ssh_key_pair" {
-  name = "ssh_key_pair_new"
-}
-
-data "aws_secretsmanager_secret_version" "ssh_key_pair_version" {
-  secret_id = data.aws_secretsmanager_secret.ssh_key_pair.id
-}
-
-locals {
-  ssh_keys_base64 = jsondecode(data.aws_secretsmanager_secret_version.ssh_key_pair_version.secret_string)
-  ssh_public_key  = base64decode(local.ssh_keys_base64.public_key)
-  ssh_private_key = base64decode(local.ssh_keys_base64.private_key)
-}
-
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = local.ssh_public_key
+
+  lifecycle {
+    ignore_changes = [public_key]
+  }
 }
 
 resource "aws_instance" "k8s_practice" {
@@ -77,9 +67,13 @@ resource "aws_instance" "k8s_practice" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = local.ssh_private_key
+      private_key = var.ssh_private_key
       host        = self.public_ip
     }
+  }
+
+  lifecycle {
+    ignore_changes = [key_name]
   }
 }
 
